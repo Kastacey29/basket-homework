fab3cf6c37c6f08ea9f9217e2a231544d685f40e
Stacey29 <Kastacey@yandex.ru>
2022-12-01 18:17:33 +0300

    first commit

---
+++ src/main/java/com/example/baskethomework/BasketHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.baskethomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/baskethomework/BasketHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baskethomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
